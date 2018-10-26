7f7aefa90e4aef57bb2bcf08f4747c837ff0bf3c
mzinkgraf <mzinkgraf@gmail.com>
2018-10-26 15:20:32 -0700

    add descriptive stats

--- R/functions.R
+++ R/functions.R
@@ -5,6 +5,84 @@
 #'
 #'This function does the opposite of in
 #'
-#' @author Matthew Zinkgraf, \email{mzinkgraf@gmail.com}
+#' @author Matthew Zinkgraf, \email{matthew.zinkgraf@wwu.edu}
 #' @export
 `%ni%`=Negate(`%in%`)
+
+
+
+
+#'
+#' Geometric mean 1
+#'
+#' @usage geo.mean1(x)
+#'
+#' @param x vector of numerical values
+#' @example
+#' #generate some data
+#' data <- rnorm(10)
+#'
+#' #calculate geomtric mean
+#' geo.mean1(data)
+#'
+#' @export
+geo.mean1 <- function(x){
+  prod(x)^(1/length(x))
+}
+
+#'
+#' Geometric mean 2
+#'
+#' @usage geo.mean2(x)
+#'
+#' @param x vector of numerical values
+#' @example
+#' #generate some data
+#' data <- rnorm(10)
+#'
+#' #calculate geomtric mean
+#' geo.mean2(data)
+#'
+#' @export
+geo.mean2 <- function(x){
+  exp(mean(log(x)))
+}
+
+#'
+#' Harmonic mean
+#'
+#' @usage har.mean(x)
+#'
+#' @param x vector of numerical values
+#' @example
+#' #generate some data
+#' data <- rnorm(10)
+#'
+#' #calculate harmonic mean
+#' har.mean(data)
+#'
+#' @export
+har.mean <- function(x){
+  length(x)/sum(1/x)
+}
+
+
+mode <- function(x){
+  as.numeric(names(sort(table(x),decreasing = T))[1])
+}
+
+#'
+#' Standard error of the mean
+#'
+#' @usage se(x)
+#'
+#' @param x vector of numerical values
+#' @example
+#' #generate some data
+#' data <- rnorm(10)
+#'
+#' #calculate the standard error
+#' se(data)
+#'
+#' @export
+se <- function(x) sd(x,na.rm = T)/sqrt(length(x[!is.na(x)]))
